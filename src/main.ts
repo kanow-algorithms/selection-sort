6553aa6f103b8a5593c35a7c11cb7f9f1c048a26
kaczor6418 <andrzej6418@gmail.com>
2019-12-13 22:57:33 +0100

    feat(selection-sort): working selection sort algortihm for ascending and descending sort direction

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,21 @@
-const test: string = 'test';
-console.log(test);
+import {SortDirection} from "./Enums";
+
+function selectionSort(elementsList: number[], direction: SortDirection = SortDirection.ASC): void {
+    const condition: (comparedElement: number, minValue: number) => boolean = direction === SortDirection.ASC ? ascendingCondition : descendingCondition;
+    for (let i: number = 0; i < elementsList.length; i++) {
+        let minValueIndex: number = i;
+        for (let j: number = i + 1; j < elementsList.length; j++) {
+            minValueIndex = condition(elementsList[j], elementsList[minValueIndex]) ? minValueIndex : j;
+        }
+        swapTwoValuesOfArray(elementsList, i, minValueIndex);
+    }
+}
+
+function swapTwoValuesOfArray(array: number[], sourceValueIndex: number, valueToSwapIndex: number) {
+    const tempValue: number = array[sourceValueIndex];
+    array[sourceValueIndex] = array[valueToSwapIndex];
+    array[valueToSwapIndex] = tempValue;
+}
+
+const ascendingCondition: (comparedElement: number, minValue: number) => boolean = (comparedElement: number, minValue: number) => comparedElement > minValue;
+const descendingCondition: (comparedElement: number, minValue: number) => boolean = (comparedElement: number, minValue: number) => comparedElement < minValue;
